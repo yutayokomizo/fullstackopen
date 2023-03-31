ad6a619a452356e600a4c7b0b72f3a1337348f6e
yutayokomizo <42861516+yutayokomizo@users.noreply.github.com>
2023-03-31 16:38:33 +0900

    Finish 2a exercises

---
+++ part1/course-info/src/components/Course.js
@@ -0,0 +1,41 @@
+const Header = (props) => {
+  return <h1>{props.name}</h1>;
+};
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.part} {props.exercises}
+    </p>
+  );
+};
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      {parts.map((part) => (
+        <Part key={part.id} part={part.name} exercises={part.exercises} />
+      ))}
+    </div>
+  );
+};
+
+const Total = ({ parts }) => {
+  return (
+    <p>
+      Numer of exercises {parts.reduce((sum, curr) => sum + curr.exercises, 0)}
+    </p>
+  );
+};
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header name={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  );
+};
+
+export default Course;
