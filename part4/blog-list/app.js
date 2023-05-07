36dc24f889fd04b67793c9057c6197cd269efba1
yutayokomizo <42861516+yutayokomizo@users.noreply.github.com>
2023-05-07 18:57:56 +0900

    Finish 4.18

--- part4/blog-list/app.js
+++ part4/blog-list/app.js
@@ -7,6 +7,7 @@ const mongoose = require('mongoose');
 const config = require('./utils/config');
 const blogsRouter = require('./controllers/blogs');
 const usersRouter = require('./controllers/users');
+const loginRouter = require('./controllers/login');
 
 const mongoUrl = config.MONGODB_URI;
 mongoose.connect(mongoUrl);
@@ -16,6 +17,7 @@ app.use(express.json());
 
 app.use('/api/blogs', blogsRouter);
 app.use('/api/users', usersRouter);
+app.use('/api/login', loginRouter);
 
 const errorHandler = (error, request, response, next) => {
   if (error.name === 'ValidationError') {
