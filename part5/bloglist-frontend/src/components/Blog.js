9997b4e668032553e85c6ddc7a82527bca411b40
yutayokomizo <42861516+yutayokomizo@users.noreply.github.com>
2023-07-04 16:54:54 +0900

    Finish 5.7

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,37 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react';
 
-export default Blog
+const Blog = ({ blog }) => {
+  const [detailVisible, setDetailVisible] = useState(false);
+
+  const showWhenVisible = { display: detailVisible ? '' : 'none' };
+  const hideWhenVisible = { display: detailVisible ? 'none' : '' };
+
+  return (
+    <div
+      style={{
+        border: '1px solid black',
+        padding: '4px 10px',
+        marginBottom: '4px',
+      }}
+    >
+      <div>
+        {blog.title} {blog.author}
+        <button style={hideWhenVisible} onClick={() => setDetailVisible(true)}>
+          view
+        </button>
+        <button style={showWhenVisible} onClick={() => setDetailVisible(false)}>
+          hide
+        </button>
+      </div>
+      <div style={showWhenVisible}>
+        <p>{blog.url}</p>
+        <div>
+          {blog.likes} <button>like</button>
+        </div>
+        <p>{blog.author}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Blog;
