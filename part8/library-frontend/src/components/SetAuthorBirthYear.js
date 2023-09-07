5f97633acedb03d60a52d2825eaa79fb4e417d66
yutayokomizo <42861516+yutayokomizo@users.noreply.github.com>
2023-09-07 20:41:37 +0100

    Finish 8.12

--- part8/library-frontend/src/components/SetAuthorBirthYear.js
+++ part8/library-frontend/src/components/SetAuthorBirthYear.js
@@ -1,14 +1,20 @@
 import { useState } from 'react';
-import { useMutation } from '@apollo/client';
+import { useMutation, useQuery } from '@apollo/client';
 import { SET_BORN_YEAR, ALL_AUTHORS } from '../queries';
 
 const SetAuthorBirthYear = () => {
   const [name, setName] = useState('');
   const [born, setBorn] = useState('');
 
+  const result = useQuery(ALL_AUTHORS);
+
   const [setBornYear] = useMutation(SET_BORN_YEAR, {
     refetchQueries: [{ query: ALL_AUTHORS }],
   });
+
+  if (result.loading) {
+    return null;
+  }
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -23,13 +29,13 @@ const SetAuthorBirthYear = () => {
     <div>
       <h3>Set birthyear</h3>
       <form onSubmit={handleSubmit}>
-        <div>
-          name{' '}
-          <input
-            value={name}
-            onChange={({ target }) => setName(target.value)}
-          />
-        </div>
+        <select value={name} onChange={({ target }) => setName(target.value)}>
+          {result.data.allAuthors.map((a) => (
+            <option key={a.name} value={a.name}>
+              {a.name}
+            </option>
+          ))}
+        </select>
         <div>
           born{' '}
           <input
