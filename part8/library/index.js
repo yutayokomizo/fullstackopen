adda11850ac2bd70e4ddcadcc85d5fd2783a7c6c
yutayokomizo <42861516+yutayokomizo@users.noreply.github.com>
2023-09-07 13:39:25 +0100

    Finish 8.3

--- part8/library/index.js
+++ part8/library/index.js
@@ -106,10 +106,18 @@ const typeDefs = `
     genres: [String]!
   }
 
+  type Author {
+    name: String!
+    id: ID!
+    born: Int
+    bookCount: Int!
+  }
+
   type Query {
     bookCount: Int!
     authorCount: Int!
     allBooks: [Book]!
+    allAuthors: [Author]!
   }
 `;
 
@@ -118,6 +126,13 @@ const resolvers = {
     bookCount: () => books.length,
     authorCount: () => authors.length,
     allBooks: () => books,
+    allAuthors: () => authors,
+  },
+  Author: {
+    bookCount: (root) => {
+      const booksByAuthor = books.filter((book) => book.author === root.name);
+      return booksByAuthor.length;
+    },
   },
 };
 
